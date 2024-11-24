a100a0dfbc6774fec04c51129e03d9c687f3cd1c
Heorhii Lishafai <heolish44@edu.gdansk.pl>
2024-11-24 13:29:37 +0100

    added title and icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: './stick.png' },
+      ],
+      title: 'IMG2Stick', // Default title
+    }
+  }
 })
